1b366d5ccdc81186b9b3bee4bbe36f3cf46b934a
Manuel Madeira <maneljssmadeira@gmail.com>
2022-08-04 23:00:46 +0100

    add seo tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,18 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <title>Aequinox</title>
+          <meta property="og:title" content="Aequinox" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://www.aequinox.io" />
+          <meta
+            property="og:image"
+            content="https://www.aequinox.io/og-image.png"
+          />
+          <meta property="og:description" content="" />
+          <meta name="twitter:card" content="summary_large_image" />
+        </Head>
         <body>
           <Main />
           <NextScript />
