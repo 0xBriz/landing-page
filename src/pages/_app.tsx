5170caedb6f64d0b6cc416d72a0ce9932733e62c
Manuel Madeira <maneljssmadeira@gmail.com>
2022-08-05 17:23:39 +0100

    add google tag script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 
 import { AppProps } from "next/app";
 import Head from "next/head";
+import Script from "next/script";
 import { Fonts } from "../components/Fonts";
 import theme from "../theme";
 
@@ -13,6 +14,21 @@ function MyApp({ Component, pageProps }: AppProps) {
       </Head>
       <Fonts />
       <Component {...pageProps} />
+
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-YVR16GE4Y6"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-YVR16GE4Y6');
+        `}
+      </Script>
     </ChakraProvider>
   );
 }
